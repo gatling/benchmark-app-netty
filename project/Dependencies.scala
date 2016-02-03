c3e941b7f71af6080de150a351eae4c91cd8c3d9
Stephane Landelle <slandelle@gatling.io>
2016-02-03 15:16:34 +0100

    Upgrade Netty 4.0.34.Final and slf4j 1.7.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,9 @@ import sbt._
 
 object Dependencies {
 
-  private val netty        = "io.netty"                      % "netty-codec-http"     % "4.0.32.Final"
+  private val netty        = "io.netty"                      % "netty-codec-http"     % "4.0.34.Final"
   private val javassist    = "org.javassist"                 % "javassist"            % "3.20.0-GA"
-  private val jzlib        = "com.jcraft"                    % "jzlib"                % "1.1.3"
-  private val slf4j        = "org.slf4j"                     % "slf4j-api"            % "1.7.10"
+  private val slf4j        = "org.slf4j"                     % "slf4j-api"            % "1.7.14"
   private val logback      = "ch.qos.logback"                % "logback-classic"      % "1.1.3"
   private val scalalogging = "com.typesafe.scala-logging"   %% "scala-logging-slf4j"  % "2.1.2"
 
