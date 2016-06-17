8c6d9a2781801140f1fce201e59481c7fdbd22de
Stephane Landelle <slandelle@gatling.io>
2016-06-17 11:00:44 +0200

    Format

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private val javassist            = "org.javassist"               % "javassist"                    % "3.20.0-GA"
   private val slf4j                = "org.slf4j"                   % "slf4j-api"                    % "1.7.14"
   private val logback              = "ch.qos.logback"              % "logback-classic"              % "1.1.3"
-  private val scalalogging =       "com.typesafe.scala-logging"   %% "scala-logging-slf4j"          % "2.1.2"
+  private val scalalogging         = "com.typesafe.scala-logging"  %% "scala-logging-slf4j"         % "2.1.2"
 
   val allDeps = Seq(netty, nettyNativeTransport, javassist, slf4j, logback, scalalogging)
 }
