3370657dd3ef591f19b967683b8a35b3d6b6575e
Stephane Landelle <slandelle@gatling.io>
2017-04-25 16:21:23 +0200

    Upgrade deps

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,7 +10,7 @@ object BuildSettings {
     organization := "io.gatling",
     resolvers    := Seq(Opts.resolver.sonatypeSnapshots),
     javaOptions  := Seq("-XX:+UseG1GC", "-Xms512M", "-Xmx512M", "-XX:+AggressiveOpts", "-XX:+OptimizeStringConcat", "-XX:+UseFastAccessorMethods", "-Djava.net.preferIPv4Stack=true", "-Djava.net.preferIPv6Addresses=false"),
-    scalaVersion := "2.12.1",
+    scalaVersion := "2.12.2",
     scalacOptions := Seq(
       "-encoding", "UTF-8",
       "-deprecation",
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,14 @@ import sbt._
 
 object Dependencies {
 
-  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.8.Final"
+  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.9.Final"
   private val nettyHandler         = "io.netty"                    % "netty-handler"                   % nettyHttp.revision
   private val nettyNativeTransport = "io.netty"                    % "netty-transport-native-epoll"    % nettyHttp.revision classifier "linux-x86_64"
   private val nettyBoringSsl       = "io.netty"                    % "netty-tcnative-boringssl-static" % "1.1.33.Fork26"
-  private val javassist            = "org.javassist"               % "javassist"                       % "3.21.0-GA"
-  private val slf4j                = "org.slf4j"                   % "slf4j-api"                       % "1.7.22"
-  private val logback              = "ch.qos.logback"              % "logback-classic"                 % "1.2.1"
+  private val slf4j                = "org.slf4j"                   % "slf4j-api"                       % "1.7.25"
+  private val logback              = "ch.qos.logback"              % "logback-classic"                 % "1.2.2"
   private val scalaLogging         = "com.typesafe.scala-logging"  %% "scala-logging"                  % "3.5.0"
   private val commonsIo            = "commons-io"                  % "commons-io"                      % "2.5"
 
-  val allDeps = Seq(nettyHttp, nettyHandler, nettyNativeTransport, nettyBoringSsl, javassist, slf4j, logback, scalaLogging, commonsIo)
+  val allDeps = Seq(nettyHttp, nettyHandler, nettyNativeTransport, nettyBoringSsl, slf4j, logback, scalaLogging, commonsIo)
 }
