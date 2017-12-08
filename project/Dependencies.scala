422e31a149c43600826bc47efe035397d3e96396
Stephane Landelle <slandelle@gatling.io>
2017-12-08 17:01:16 +0100

    Upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object Dependencies {
 
-  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.9.Final"
+  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.18.Final"
   private val nettyHandler         = "io.netty"                    % "netty-handler"                   % nettyHttp.revision
   private val nettyNativeTransport = "io.netty"                    % "netty-transport-native-epoll"    % nettyHttp.revision classifier "linux-x86_64"
-  private val nettyBoringSsl       = "io.netty"                    % "netty-tcnative-boringssl-static" % "1.1.33.Fork26"
+  private val nettyBoringSsl       = "io.netty"                    % "netty-tcnative-boringssl-static" % "2.0.6.Final"
   private val slf4j                = "org.slf4j"                   % "slf4j-api"                       % "1.7.25"
-  private val logback              = "ch.qos.logback"              % "logback-classic"                 % "1.2.2"
-  private val scalaLogging         = "com.typesafe.scala-logging"  %% "scala-logging"                  % "3.5.0"
-  private val commonsIo            = "commons-io"                  % "commons-io"                      % "2.5"
+  private val logback              = "ch.qos.logback"              % "logback-classic"                 % "1.2.3"
+  private val scalaLogging         = "com.typesafe.scala-logging"  %% "scala-logging"                  % "3.7.2"
+  private val commonsIo            = "commons-io"                  % "commons-io"                      % "2.6"
 
   val allDeps = Seq(nettyHttp, nettyHandler, nettyNativeTransport, nettyBoringSsl, slf4j, logback, scalaLogging, commonsIo)
 }
