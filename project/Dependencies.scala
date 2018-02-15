8ff2d1f4147a793a98943b1cde2747cc48a65bc6
Stephane Landelle <slandelle@gatling.io>
2018-02-15 16:33:27 +0100

    Update deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object Dependencies {
 
-  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.18.Final"
+  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.21.Final"
   private val nettyHandler         = "io.netty"                    % "netty-handler"                   % nettyHttp.revision
   private val nettyNativeTransport = "io.netty"                    % "netty-transport-native-epoll"    % nettyHttp.revision classifier "linux-x86_64"
-  private val nettyBoringSsl       = "io.netty"                    % "netty-tcnative-boringssl-static" % "2.0.6.Final"
+  private val nettyBoringSsl       = "io.netty"                    % "netty-tcnative-boringssl-static" % "2.0.7.Final"
   private val slf4j                = "org.slf4j"                   % "slf4j-api"                       % "1.7.25"
   private val logback              = "ch.qos.logback"              % "logback-classic"                 % "1.2.3"
   private val scalaLogging         = "com.typesafe.scala-logging"  %% "scala-logging"                  % "3.7.2"
