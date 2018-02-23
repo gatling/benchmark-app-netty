dfd7cdefa1bf3a3cf6a24a2eef24ef599618000c
Stephane Landelle <slandelle@gatling.io>
2018-02-23 17:08:15 +0100

    Upgrade netty 4.1.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.21.Final"
+  private val nettyHttp            = "io.netty"                    % "netty-codec-http"                % "4.1.22.Final"
   private val nettyHandler         = "io.netty"                    % "netty-handler"                   % nettyHttp.revision
   private val nettyNativeTransport = "io.netty"                    % "netty-transport-native-epoll"    % nettyHttp.revision classifier "linux-x86_64"
   private val nettyBoringSsl       = "io.netty"                    % "netty-tcnative-boringssl-static" % "2.0.7.Final"
