c03d7cfaf0669af0d7b5cc7d27ae77c2074bba4a
Stephane Landelle <slandelle@gatling.io>
2020-03-22 22:06:16 +0100

    Enable both HTTP and HTTPS

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-Server runs on port 8000.
-
-Run with options:
-
-`sbt -Dgatling.useNativeTransport=true -Dgatling.useHttps=true -Dgatling.useOpenSsl=true run`
+`sbt run`
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,10 +1,10 @@
 import java.io.{ByteArrayOutputStream, IOException}
 import java.net.InetSocketAddress
+import java.nio.charset.StandardCharsets.UTF_8
 import java.util.concurrent.TimeUnit
 import java.util.zip.GZIPOutputStream
 
-import scala.io.{Codec, Source}
-
+import scala.io.Source
 import com.typesafe.scalalogging.StrictLogging
 import io.netty.bootstrap.ServerBootstrap
 import io.netty.buffer.Unpooled
@@ -20,21 +20,29 @@ import io.netty.handler.ssl.util.SelfSignedCertificate
 import io.netty.handler.ssl.{SslContextBuilder, SslProvider}
 import io.netty.handler.timeout.{IdleState, IdleStateEvent}
 import io.netty.util._
+import io.netty.util.internal.PlatformDependent
 
 object Server extends StrictLogging {
 
-  implicit val codec: Codec = Codec.UTF8
-
   private val HtmlContentType = new AsciiString("text/html; charset=utf-8")
 
-  private val port = 8000
+  private val httpPort = 8000
+  private val httpsPort = 8001
 
   object Content {
     def fromText(text: String, contentType: CharSequence): Content =
-      new Content(text.getBytes(codec.charSet), contentType)
+      new Content(text.getBytes(UTF_8), contentType)
 
     def fromResource(res: String, contentType: CharSequence): Content =
       new Content(resourceAsBytes(res), contentType)
+
+    val HelloWorld = Content.fromText("Hello, World!", TEXT_PLAIN)
+    val Json100 = Content.fromResource("100.json", APPLICATION_JSON)
+    val Json250 = Content.fromResource("250.json", APPLICATION_JSON)
+    val Json500 = Content.fromResource("500.json", APPLICATION_JSON)
+    val Json1k = Content.fromResource("1k.json", APPLICATION_JSON)
+    val Json10k = Content.fromResource("10k.json", APPLICATION_JSON)
+    val News = Content.fromResource("news.html", HtmlContentType)
   }
 
   case class Content(rawBytes: Array[Byte], contentType: CharSequence) {
@@ -47,39 +55,24 @@ object Server extends StrictLogging {
     }
   }
 
-  private val HelloWorldContent = Content.fromText("Hello, World!", TEXT_PLAIN)
-  private val Json100Content = Content.fromResource("100.json", APPLICATION_JSON)
-  private val Json250Content = Content.fromResource("250.json", APPLICATION_JSON)
-  private val Json500Content = Content.fromResource("500.json", APPLICATION_JSON)
-  private val Json1kContent = Content.fromResource("1k.json", APPLICATION_JSON)
-  private val Json10kContent = Content.fromResource("10k.json", APPLICATION_JSON)
-  private val NewsContent = Content.fromResource("news.html", HtmlContentType)
-
   private def resourceAsBytes(path: String) = {
     val source = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(path))
     try {
-      source.mkString.getBytes(codec.charSet)
+      source.mkString.getBytes(UTF_8)
     } finally {
       source.close()
     }
   }
 
   private def writeResponse(ctx: ChannelHandlerContext, response: DefaultFullHttpResponse): Unit = {
-    if (
-      !response.headers().contains(CONTENT_LENGTH) &&
-        !response.headers().contains(TRANSFER_ENCODING)) {
-      response.headers().set(CONTENT_LENGTH, response.content.readableBytes)
-    }
-
+    response.headers.set(CONTENT_LENGTH, response.content.readableBytes)
     ctx.writeAndFlush(response)
     logger.debug(s"wrote response=$response")
   }
 
   private def writeResponse(ctx: ChannelHandlerContext, request: HttpRequest, content: Content): Unit = {
-
     val compress = acceptGzip(request)
     val bytes = if (compress) content.compressedBytes else content.rawBytes
-
     val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(bytes))
 
     response.headers
@@ -114,9 +107,7 @@ object Server extends StrictLogging {
     logger.info(s"os.version ${System.getProperty("os.version")}")
 
     ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.DISABLED)
-    val useNativeTransport = java.lang.Boolean.getBoolean("gatling.useNativeTransport")
-    val useHttps = java.lang.Boolean.getBoolean("gatling.useHttps")
-    val useOpenSsl = sys.props.get("gatling.useOpenSsl").map(_.toBoolean).getOrElse(true)
+    val useNativeTransport = !PlatformDependent.isOsx && !PlatformDependent.isWindows
 
     val bossGroup = if (useNativeTransport) new EpollEventLoopGroup else new NioEventLoopGroup
     val workerGroup = if (useNativeTransport) new EpollEventLoopGroup else new NioEventLoopGroup
@@ -124,85 +115,82 @@ object Server extends StrictLogging {
     val ssc = new SelfSignedCertificate
     val sslContext = SslContextBuilder
       .forServer(ssc.certificate, ssc.privateKey)
-      .sslProvider(if (useOpenSsl) SslProvider.OPENSSL else SslProvider.JDK)
+      .sslProvider(SslProvider.OPENSSL)
+      .protocols("TLSv1.3")
       .build()
 
     val channelClass: Class[_ <: ServerSocketChannel] = if (useNativeTransport) classOf[EpollServerSocketChannel] else classOf[NioServerSocketChannel]
+
+    def channelInitializer(useHttps: Boolean): ChannelInitializer[Channel] =
+      (ch: Channel) => {
+        val pipeline = ch.pipeline
+        if (useHttps) {
+          pipeline.addLast(sslContext.newHandler(ch.alloc))
+        }
+        pipeline
+          .addLast("idleTimer", new CloseOnIdleReadTimeoutHandler(5))
+          .addLast("decoder", new HttpRequestDecoder(4096, 8192, 8192, false))
+          .addLast("aggregator", new HttpObjectAggregator(30000))
+          .addLast("encoder", new HttpResponseEncoder)
+          .addLast("handler", new ChannelInboundHandlerAdapter {
+
+            override def userEventTriggered(ctx: ChannelHandlerContext, evt: AnyRef): Unit =
+              evt match {
+                case e: IdleStateEvent if e.state == IdleState.READER_IDLE =>
+                  logger.info("Idle => closing")
+                  ctx.close()
+                case _ =>
+              }
+
+            override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = cause match {
+              case ioe: IOException =>
+                if (ioe.getMessage != "Connection reset by peer") {
+                  // ignore, this is just client aborting
+                  ioe.printStackTrace()
+                }
+                ctx.channel.close()
+              case _ => ctx.fireExceptionCaught(cause)
+            }
+
+            override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit =
+              msg match {
+                case request: FullHttpRequest =>
+                  ReferenceCountUtil.release(request)
+
+                  if (request.uri == "/echo") {
+                    val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, request.content)
+                    writeResponse(ctx, response)
+                  } else {
+                    request.uri match {
+                      case "/hello" => writeResponse(ctx, request, Content.HelloWorld)
+                      case "/json100" => writeResponse(ctx, request, Content.Json100)
+                      case "/json250" => writeResponse(ctx, request, Content.Json250)
+                      case "/json500" => writeResponse(ctx, request, Content.Json500)
+                      case "/json1k" => writeResponse(ctx, request, Content.Json1k)
+                      case "/json10k" => writeResponse(ctx, request, Content.Json10k)
+                      case "/news" => writeResponse(ctx, request, Content.News)
+                      case "/post" => writeResponse(ctx, request, Content.HelloWorld)
+                      case _ => writeResponse(ctx, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND))
+                    }
+                  }
+
+                case _ =>
+                  logger.error(s"Read unexpected msg=$msg")
+              }
+          })
+      }
 
     val bootstrap = new ServerBootstrap()
       .option[Integer](ChannelOption.SO_BACKLOG, 2 * 1024)
       .group(bossGroup, workerGroup)
       .channel(channelClass)
-      .childHandler(new ChannelInitializer[Channel] {
-        override def initChannel(ch: Channel): Unit = {
-          val pipeline = ch.pipeline
-          if (useHttps) {
-            pipeline.addLast(sslContext.newHandler(ch.alloc))
-          }
-          pipeline
-            // don't validate headers
-            //            .addLast("idleTimer", new CloseOnIdleReadTimeoutHandler(1))
-            .addLast("decoder", new HttpRequestDecoder(4096, 8192, 8192, false))
-            .addLast("aggregator", new HttpObjectAggregator(30000))
-            .addLast("encoder", new HttpResponseEncoder)
-            .addLast("handler", new ChannelInboundHandlerAdapter {
+      .childHandler(channelInitializer(useHttps = false))
 
-              override def userEventTriggered(ctx: ChannelHandlerContext, evt: AnyRef): Unit =
-                evt match {
-                  case e: IdleStateEvent if e.state == IdleState.READER_IDLE =>
-                    logger.info("Idle => closing")
-                    ctx.close()
-                  case _ =>
-                }
-
-              override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = cause match {
-                case ioe: IOException =>
-                  if (ioe.getMessage != "Connection reset by peer") {
-                    // ignore, this is just Gatling aborting
-                    ioe.printStackTrace()
-                  }
-                  ctx.channel.close()
-                case _ => ctx.fireExceptionCaught(cause)
-              }
-
-              override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit =
-                msg match {
-                  case request: FullHttpRequest =>
-                    ReferenceCountUtil.release(request) // FIXME is this necessary?
-
-                    if (request.uri == "/echo") {
-                      val content = request.content()
-                      val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content)
-                      response.headers().add(CONTENT_LENGTH, content.readableBytes)
-                      writeResponse(ctx, response)
-                    } else {
-                      request.uri match {
-                        case "/hello" => writeResponse(ctx, request, HelloWorldContent)
-                        case "/json100" => writeResponse(ctx, request, Json100Content)
-                        case "/json250" => writeResponse(ctx, request, Json250Content)
-                        case "/json500" => writeResponse(ctx, request, Json500Content)
-                        case "/json1k" => writeResponse(ctx, request, Json1kContent)
-                        case "/json10k" => writeResponse(ctx, request, Json10kContent)
-                        case "/news" => writeResponse(ctx, request, NewsContent)
-                        case "/post" => writeResponse(ctx, request, HelloWorldContent)
-                        case s if s.startsWith("/infinite-redirect") =>
-                          val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.TEMPORARY_REDIRECT)
-                          response.headers().set(HttpHeaderNames.LOCATION, "/infinite-redirect")
-                          writeResponse(ctx, response)
-                        case _ => writeResponse(ctx, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND))
-                      }
-                    }
-
-                  case _ =>
-                    logger.error(s"Read unexpected msg=$msg")
-                }
-            })
-        }
-      })
-
-    val f = bootstrap.bind(new InetSocketAddress(port)).sync
-    logger.info("Server started on port " + port)
-    f.channel.closeFuture.sync
+    val whenCloseHttp = bootstrap.bind(new InetSocketAddress(httpPort)).sync
+    val whenCloseHttps = bootstrap.clone.childHandler(channelInitializer(useHttps = true)).bind(new InetSocketAddress(httpsPort)).sync
+    logger.info(s"Server started on port $httpPort (HTTP) and port $httpsPort (HTTPS)")
+    whenCloseHttp.channel.closeFuture.sync
+    whenCloseHttps.channel.closeFuture.sync
     logger.info("stopping")
     bossGroup.shutdownGracefully()
     workerGroup.shutdownGracefully()
