edc3c3d33a53c7755a4e36e8b22acc70b7a920fd
Stephane Landelle <slandelle@gatling.io>
2023-10-01 22:59:04 +0200

    Use a @Sharable singleton for the applicative handler

--- src/main/scala/Http.scala
+++ src/main/scala/Http.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.{ForkJoinPool, TimeUnit}
 import com.typesafe.scalalogging.StrictLogging
 import io.netty.bootstrap.ServerBootstrap
 import io.netty.buffer.Unpooled
+import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel._
 import io.netty.handler.codec.http._
 import io.netty.handler.codec.http.HttpHeaderNames.{ACCEPT_ENCODING, CONTENT_ENCODING, CONTENT_LENGTH, CONTENT_TYPE}
@@ -16,13 +17,11 @@ import org.apache.commons.math3.distribution.LogNormalDistribution
 import scala.jdk.CollectionConverters._
 import scala.concurrent.duration.FiniteDuration
 
-object Http {
+@Sharable
+object AppHandler extends ChannelInboundHandlerAdapter with StrictLogging {
+
   private val XDelayHeader = new AsciiString("X-Delay")
   private val XUseLogNormalDelayHeader = new AsciiString("X-UseLogNormalDelay")
-}
-
-final class Http(clearPort: Int, securedPort: Int, sslContext: SslContext, readIdleTimeout: FiniteDuration) extends StrictLogging {
-
   private val logNormalDistribution = new LogNormalDistribution()
 
   private def writeResponse(ctx: ChannelHandlerContext, response: DefaultFullHttpResponse): Unit = {
@@ -33,8 +32,8 @@ final class Http(clearPort: Int, securedPort: Int, sslContext: SslContext, readI
 
   private def writeResponse(ctx: ChannelHandlerContext, request: HttpRequest, content: Content): Unit = {
     val acceptGzip = Option(request.headers.get(ACCEPT_ENCODING)).exists(_.contains("gzip"))
-    val maybeDelay = Option(request.headers.get(Http.XDelayHeader)).map(_.toLong)
-    val useLogNormalDelay = Option(request.headers.get(Http.XUseLogNormalDelayHeader)).exists(_.toBoolean)
+    val maybeDelay = Option(request.headers.get(AppHandler.XDelayHeader)).map(_.toLong)
+    val useLogNormalDelay = Option(request.headers.get(AppHandler.XUseLogNormalDelayHeader)).exists(_.toBoolean)
 
     val bytes = if (acceptGzip) content.compressedBytes else content.rawBytes
     val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(bytes))
@@ -49,7 +48,7 @@ final class Http(clearPort: Int, securedPort: Int, sslContext: SslContext, readI
 
     maybeDelay match {
       case Some(averageDelay) =>
-        val delay = if (useLogNormalDelay) math.round(logNormalDistribution.sample * averageDelay) else averageDelay
+        val delay = if (useLogNormalDelay) math.round(AppHandler.logNormalDistribution.sample * averageDelay) else averageDelay
         ctx.executor.schedule(new Runnable {
           override def run(): Unit =
             if (ctx.channel.isActive) {
@@ -62,6 +61,83 @@ final class Http(clearPort: Int, securedPort: Int, sslContext: SslContext, readI
     }
   }
 
+  override def userEventTriggered(ctx: ChannelHandlerContext, evt: AnyRef): Unit =
+    evt match {
+      case e: IdleStateEvent if e.state == IdleState.READER_IDLE =>
+        logger.info("Idle => closing")
+        ctx.close()
+      case _ =>
+    }
+
+  override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = cause match {
+    case ioe: IOException =>
+      val root = if (ioe.getCause != null) ioe.getCause else ioe
+      if (!(root.getMessage != null && root.getMessage.endsWith("Connection reset by peer"))) {
+        // ignore, this is just client aborting socket
+        logger.error("exceptionCaught", ioe)
+      }
+      ctx.channel.close()
+    case _ => ctx.fireExceptionCaught(cause)
+  }
+
+  override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit =
+    try {
+      msg match {
+        case request: FullHttpRequest =>
+          if (request.uri == "/echo") {
+            val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, request.content.retain())
+            writeResponse(ctx, response)
+
+          } else if (request.uri == "/redirect/infinite") {
+            val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.FOUND)
+            response.headers.add(HttpHeaderNames.LOCATION, "/redirect/infinite")
+            writeResponse(ctx, response)
+
+          } else if (request.uri == "/redirect/endpoint") {
+            val redirectCookie = Option(request.headers.get(HttpHeaderNames.COOKIE))
+              .map(cookie.ServerCookieDecoder.STRICT.decode)
+              .flatMap(_.asScala.collectFirst { case cookie if cookie.name == "RedirectCookie" => cookie.value.toInt })
+              .getOrElse(0)
+
+            val newRedirectCookie = new cookie.DefaultCookie("RedirectCookie", (redirectCookie + 1).toString)
+            newRedirectCookie.setPath("/redirect")
+
+            val doRedirect = redirectCookie % 2 == 0
+
+            val statusCode = if (doRedirect) HttpResponseStatus.FOUND else HttpResponseStatus.OK
+            val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, statusCode)
+            response.headers.add(HttpHeaderNames.SET_COOKIE, cookie.ServerCookieEncoder.STRICT.encode(newRedirectCookie))
+
+            if (doRedirect) {
+              response.headers.add(HttpHeaderNames.LOCATION, "/redirect/endpoint")
+            }
+
+            writeResponse(ctx, response)
+
+          } else {
+            request.uri match {
+              case Content.HelloWorld.path => writeResponse(ctx, request, Content.HelloWorld)
+              case Content.Json100.path => writeResponse(ctx, request, Content.Json100)
+              case Content.Json250.path => writeResponse(ctx, request, Content.Json250)
+              case Content.Json500.path => writeResponse(ctx, request, Content.Json500)
+              case Content.Json1k.path => writeResponse(ctx, request, Content.Json1k)
+              case Content.Json10k.path => writeResponse(ctx, request, Content.Json10k)
+              case Content.Html46k.path => writeResponse(ctx, request, Content.Html46k)
+              case Content.Html232k.path => writeResponse(ctx, request, Content.Html232k)
+              case _ => writeResponse(ctx, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND))
+            }
+          }
+
+        case _ =>
+          logger.error(s"Read unexpected msg=$msg")
+      }
+    } finally {
+      ReferenceCountUtil.release(msg)
+    }
+}
+
+final class Http(clearPort: Int, securedPort: Int, sslContext: SslContext, readIdleTimeout: FiniteDuration) extends StrictLogging {
+
   private def httpChannelInitializer(sslContext: Option[SslContext]): ChannelInitializer[Channel] =
     (ch: Channel) => {
       val pipeline = ch.pipeline
@@ -71,82 +147,7 @@ final class Http(clearPort: Int, securedPort: Int, sslContext: SslContext, readI
         .addLast("decoder", new HttpRequestDecoder(4096, 8192, 8192, false))
         .addLast("aggregator", new HttpObjectAggregator(30000))
         .addLast("encoder", new HttpResponseEncoder)
-        .addLast("handler", new ChannelInboundHandlerAdapter {
-
-          override def userEventTriggered(ctx: ChannelHandlerContext, evt: AnyRef): Unit =
-            evt match {
-              case e: IdleStateEvent if e.state == IdleState.READER_IDLE =>
-                logger.info("Idle => closing")
-                ctx.close()
-              case _ =>
-            }
-
-          override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = cause match {
-            case ioe: IOException =>
-              val root = if (ioe.getCause != null) ioe.getCause else ioe
-              if (!(root.getMessage != null && root.getMessage.endsWith("Connection reset by peer"))) {
-                // ignore, this is just client aborting socket
-                logger.error("exceptionCaught", ioe)
-              }
-              ctx.channel.close()
-            case _ => ctx.fireExceptionCaught(cause)
-          }
-
-          override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit =
-          try {
-            msg match {
-              case request: FullHttpRequest =>
-                if (request.uri == "/echo") {
-                  val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, request.content.retain())
-                  writeResponse(ctx, response)
-
-                } else if (request.uri == "/redirect/infinite") {
-                  val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.FOUND)
-                  response.headers.add(HttpHeaderNames.LOCATION, "/redirect/infinite")
-                  writeResponse(ctx, response)
-
-                } else if (request.uri == "/redirect/endpoint") {
-                  val redirectCookie = Option(request.headers.get(HttpHeaderNames.COOKIE))
-                    .map(cookie.ServerCookieDecoder.STRICT.decode)
-                    .flatMap(_.asScala.collectFirst{ case cookie if cookie.name == "RedirectCookie" => cookie.value.toInt})
-                    .getOrElse(0)
-
-                  val newRedirectCookie = new cookie.DefaultCookie("RedirectCookie", (redirectCookie + 1).toString)
-                  newRedirectCookie.setPath("/redirect")
-
-                  val doRedirect = redirectCookie % 2 == 0
-
-                  val statusCode = if (doRedirect) HttpResponseStatus.FOUND else HttpResponseStatus.OK
-                  val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, statusCode)
-                  response.headers.add(HttpHeaderNames.SET_COOKIE, cookie.ServerCookieEncoder.STRICT.encode(newRedirectCookie))
-
-                  if (doRedirect) {
-                    response.headers.add(HttpHeaderNames.LOCATION, "/redirect/endpoint")
-                  }
-
-                  writeResponse(ctx, response)
-
-                } else {
-                  request.uri match {
-                    case Content.HelloWorld.path => writeResponse(ctx, request, Content.HelloWorld)
-                    case Content.Json100.path => writeResponse(ctx, request, Content.Json100)
-                    case Content.Json250.path => writeResponse(ctx, request, Content.Json250)
-                    case Content.Json500.path => writeResponse(ctx, request, Content.Json500)
-                    case Content.Json1k.path => writeResponse(ctx, request, Content.Json1k)
-                    case Content.Json10k.path => writeResponse(ctx, request, Content.Json10k)
-                    case Content.Html46k.path => writeResponse(ctx, request, Content.Html46k)
-                    case Content.Html232k.path => writeResponse(ctx, request, Content.Html232k)
-                    case _ => writeResponse(ctx, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND))
-                  }
-                }
-
-              case _ =>
-                logger.error(s"Read unexpected msg=$msg")
-            }
-          } finally {
-            ReferenceCountUtil.release(msg)
-          }
-        })
+        .addLast("handler", AppHandler)
     }
 
   def boot(bootstrap: ServerBootstrap): Seq[ChannelFuture] =
